e7c268a85ecbd2da3ba0ddbd5523f16e7b2702c8
Rameez Bhatti <xrkalix@gmail.com>
2022-07-20 19:14:08 +0500

    traversy practice

--- index.php
+++ index.php
@@ -1,66 +1,77 @@
+<?php 
+
+phpinfo();exit;
+
+?>
+
 <html>
+
 <head>
-<title>Here I am.</title>
-<style>
-    .ui-datepicker-calendar {
-        display: none;
-    }
-</style>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/datepicker/1.0.10/datepicker.min.css" rel="stylesheet" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datepicker/1.0.10/datepicker.min.js" integrity="sha512-RCgrAvvoLpP7KVgTkTctrUdv7C6t7Un3p1iaoPr1++3pybCyCsCZZN7QEHMZTcJTmcJ7jzexTO+eFpHk4OCFAg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <title>Here I am.</title>
+    <style>
+        .ui-datepicker-calendar {
+            display: none;
+        }
+
+    </style>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/datepicker/1.0.10/datepicker.min.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datepicker/1.0.10/datepicker.min.js" integrity="sha512-RCgrAvvoLpP7KVgTkTctrUdv7C6t7Un3p1iaoPr1++3pybCyCsCZZN7QEHMZTcJTmcJ7jzexTO+eFpHk4OCFAg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
 </head>
+
 <body>
-<div>
-    <label>Date:</label>
-    <input id="datepicker" type="text" class="datepicker" value="2003-12-20"> 
-</div>
+    <div>
+        <label>Date:</label>
+        <input id="datepicker" type="text" class="datepicker" value="2003-12-20">
+    </div>
 </body>
 <script type="text/javascript">
     let element = $('.datepicker');
-     element.datepicker();
-//    let date_format = "yy-mm-dd";
-//    let date_element = element.hasClass('datepicker');
-//    var clone = element.clone();
-//    let date_range = "1900:2030";
-//    let this_value = element.val();
-//        let date_format_mapping = {
-//            'YYYY-MM-DD' : 'yy-mm-dd',
-//            'DD/MM/YYYY' : 'dd/mm/yy',
-//            'MM/DD/YYYY' : 'mm/dd/yy'
-//        };
-//        let datepicker_date_format = 'yy-mm-dd';
-//        if(date_format_mapping[date_format]!=undefined){
-//            datepicker_date_format = date_format_mapping[date_format];
-//        }
-//        element.datepicker();
-//            dateFormat: datepicker_date_format,
-//            yearRange: date_range,
-//            changeMonth: true,
-//            changeYear: true,
-//            beforeShow: function (input, inst) {
-//                // adjust position of datepicker
-//                var rect = input.getBoundingClientRect();
-//                setTimeout(function () {
-//                    inst.dpDiv.css({ top: rect.top + 40, left: rect.left + 0 });
-//                }, 0);
-//            }
-//        });
-//        let alt_id = element.attr('id') + '-alt';
-//        clone.attr('id', alt_id);
-//        if (!date_element) {
-//            clone.insertAfter(org_element);
-//            clone.hide();
-//        }
-//        element.datepicker( "option", "altField", "#" + alt_id);
-//        element.datepicker( "option", "altFormat", "yy-mm-dd");
-//        if (this_value!=""){
-//            let check_date_format =  moment(this_value, 'YYYY-MM-DD', true).isValid()
-//            if (check_date_format){
-//                var myDate = moment(this_value, 'YYYY-MM-DD').toDate();
-//                element.datepicker("setDate", new Date(myDate));
-//            }
-//        }
+    element.datepicker();
+    //    let date_format = "yy-mm-dd";
+    //    let date_element = element.hasClass('datepicker');
+    //    var clone = element.clone();
+    //    let date_range = "1900:2030";
+    //    let this_value = element.val();
+    //        let date_format_mapping = {
+    //            'YYYY-MM-DD' : 'yy-mm-dd',
+    //            'DD/MM/YYYY' : 'dd/mm/yy',
+    //            'MM/DD/YYYY' : 'mm/dd/yy'
+    //        };
+    //        let datepicker_date_format = 'yy-mm-dd';
+    //        if(date_format_mapping[date_format]!=undefined){
+    //            datepicker_date_format = date_format_mapping[date_format];
+    //        }
+    //        element.datepicker();
+    //            dateFormat: datepicker_date_format,
+    //            yearRange: date_range,
+    //            changeMonth: true,
+    //            changeYear: true,
+    //            beforeShow: function (input, inst) {
+    //                // adjust position of datepicker
+    //                var rect = input.getBoundingClientRect();
+    //                setTimeout(function () {
+    //                    inst.dpDiv.css({ top: rect.top + 40, left: rect.left + 0 });
+    //                }, 0);
+    //            }
+    //        });
+    //        let alt_id = element.attr('id') + '-alt';
+    //        clone.attr('id', alt_id);
+    //        if (!date_element) {
+    //            clone.insertAfter(org_element);
+    //            clone.hide();
+    //        }
+    //        element.datepicker( "option", "altField", "#" + alt_id);
+    //        element.datepicker( "option", "altFormat", "yy-mm-dd");
+    //        if (this_value!=""){
+    //            let check_date_format =  moment(this_value, 'YYYY-MM-DD', true).isValid()
+    //            if (check_date_format){
+    //                var myDate = moment(this_value, 'YYYY-MM-DD').toDate();
+    //                element.datepicker("setDate", new Date(myDate));
+    //            }
+    //        }
+
 </script>
+
 </html>
---
+++ php_traversy/index.php
@@ -0,0 +1,48 @@
+<?php 
+
+$a = [1,2,3];
+
+// var_dump($a);
+
+// var_export($a);
+// var_export($a);
+// var_export($a);
+
+
+/* --------- PHP Data Types --------- */
+/*
+- String - A string is a series of characters surrounded by quotes
+- Integer - Whole numbers
+- Float - Decimal numbers
+- Boolean - true or false
+- Array - An array is a special variable, which can hold more than one value
+- Object - A class
+- NULL - Empty variable
+- Resource - A special variable that holds a resource
+*/
+
+/* --------- Variable Rules --------- */
+/*
+- Variables must be prefixed with $
+- Variables must start with a letter or the underscore character
+- variables can't start with a number
+- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variables are case-sensitive ($name and $NAME are two different variables)
+*/
+
+$name = 'Rameez';
+$age = 34;
+
+
+echo "$name is $age old.";
+
+
+
+
+
+?>
+
+<html>
+<head><title>PHP Practice</title></head>
+<body><h1></h1></body>
+</html>
